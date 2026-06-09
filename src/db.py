6a61948f41e2125f0bfa6ab696fd500c38abe859
Pralay Ahluwalia <Pralay.Ahluwalia@data-business.de>
2026-06-09 16:38:17 +0200

    Commit

--- src/db.py
+++ src/db.py
@@ -200,6 +200,7 @@ class DatabricksSqlBackend:
             "access_token": access_token,
         }
         LOGGER.info("Connection parameters (excluding token): %s", {k: v for k, v in connect_kwargs.items() if k != "access_token"})
+        LOGGER.info("Token length: %d characters", len(access_token))
         if self._config.default_catalog:
             connect_kwargs["catalog"] = self._config.default_catalog
         if self._config.default_schema:
